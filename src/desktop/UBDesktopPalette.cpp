fcc5ede0b04c4624bd8dea78b102dc72cad41d64
habruening <hbrng.computer@mailbox.org>
2022-06-26 10:09:57 +0200

    This has to be activated still.

--- src/desktop/UBDesktopPalette.cpp
+++ src/desktop/UBDesktopPalette.cpp
@@ -201,7 +201,7 @@ void UBDesktopPalette::maximizeMe()
     adjustSizeAndPosition();
 
     // Notify that the maximization has been done
-    emit maximized();
+    onDesktopPaletteMaximized()
 #ifdef UB_REQUIRES_MASK_UPDATE
         emit refreshMask();
 #endif
@@ -269,6 +269,7 @@ void UBDesktopPalette::minimizePalette(const QPoint& pos)
     if(mMinimizedLocation != eMinimizedLocation_None)
     {
         minimizeMe(mMinimizedLocation);
+        onDesktopPaletteMinimized();
     }
     }
     else
